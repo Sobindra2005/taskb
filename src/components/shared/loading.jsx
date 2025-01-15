735479dfe8d4df550f698b5b821a4e27e355ce2d
sobindra2005 <sobhindra2005@gmail.com>
2025-01-15 17:58:07 +0545

    Add framer-motion and motion libraries, implement loading and dropdown components, and update header for filtering tasks

---
+++ src/components/shared/loading.jsx
@@ -0,0 +1,16 @@
+export const Loading = () => {
+    return (
+        <main className="p-4  flex  justify-center mt-5 gap-16">
+            <div className="card bg-gray-700 shadow-md rounded-lg p-4 mb-4 min-w-[20rem] min-h-[20rem] animate-pulse">
+
+            </div>
+            <div className="card bg-gray-700 shadow-md rounded-lg p-4 mb-4 min-w-[20rem] min-h-[20rem] animate-pulse">
+
+            </div>
+            <div className="card bg-gray-700 shadow-md rounded-lg p-4 mb-4 min-w-[20rem] min-h-[20rem] animate-pulse">
+
+            </div>
+
+        </main>
+    )
+}
